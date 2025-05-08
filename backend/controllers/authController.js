3adf1f1952ac98e853f04646bcbdee14c7dd66b4
uttamnepal765@gmail.com <uttamnepal765@gmail.com>
2025-05-08 12:55:10 +1000

    for changes

---
+++ backend/controllers/authController.js
@@ -0,0 +1,35 @@
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+
+exports.register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const user = new User({ name, email, password });
+    await user.save();
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    res.status(201).json({ token });
+  } catch (err) {
+    res.status(400).json({ error: "Registration failed" });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      return res.status(400).json({ error: "Invalid email or password" });
+    }
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    res.status(200).json({ token });
+  } catch (err) {
+    res.status(400).json({ error: "Login failed" });
+  }
+};
