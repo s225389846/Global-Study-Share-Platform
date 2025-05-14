4f82e65dbb57a4fcbfc0cbf427600533d00cf04b
Trilochan Adhikari <s225389846@deakin.edu.au>
2025-05-14 17:53:32 +1000

    Login integration

--- public/JS/scripts.js
+++ public/JS/scripts.js
@@ -51,3 +51,37 @@ document
       alert("Something went wrong. Please try again.");
     }
   });
+
+// Login Integration from backend
+
+document
+  .getElementById("login-form")
+  .addEventListener("submit", async function (event) {
+    event.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    try {
+      const response = await fetch("/api/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        localStorage.setItem("token", data.token);
+        window.location.href = "/templates/user/user-reistration.html";
+      } else {
+        // Handle login error
+        alert(data.message || "Login failed");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      alert("An error occurred. Please try again.");
+    }
+  });
