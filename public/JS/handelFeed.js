7a185d467ed47311b5879aafeafb9dea7d605943
Trilochan Adhikari <s225389846@deakin.edu.au>
2025-05-24 22:09:23 +1000

    minor issue fixed

--- public/JS/handelFeed.js
+++ public/JS/handelFeed.js
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", async () => {
     }
 
     const result = await response.json();
-    const questions = result.data; // ✅ Extract the `data` array
+    const questions = result.data;
 
     console.log("Fetched questions:", questions);
 
@@ -53,7 +53,36 @@ document.addEventListener("DOMContentLoaded", async () => {
               ${question.body}
             </div>
             <div class="feed-bottom">
-              <button class="btn cmt">Comment</button>
+              <button type="button" class="btn cmt"  data-bs-toggle="modal" data-bs-target="#staticBackdrop">Comment</button>
+              <!-- Modal -->
+              <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-scrollable">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h5 class="modal-title" id="staticBackdropLabel">Comments</h5>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                      <div class="user-comment"> 
+                        <div class="user-name"> <i class="fas fa-user-circle"></i> Trilochan </div>
+                        <div clasS="user-ans"> This is the answer. </div>
+                      </div>
+                      <div class="user-comment"> 
+                        <div class="user-name"> <i class="fas fa-user-circle"></i> Trilochan </div>
+                        <div clasS="user-ans"> This is the answer. </div>
+                      </div>
+                    </div>
+                    <div class="modal-footer">
+                    <div class="chat-input-area" data-dashlane-rid="832076f5afbd1aca" data-dashlane-classification="other">
+                     <form> 
+                        <input type="text" id="user-comments" placeholder="Type your comment...">
+                        <button>Send</button>
+                     </form>
+                    </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
               <button class="btn cmt">Report</button>
             </div>
           </div>
